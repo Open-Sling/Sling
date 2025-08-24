e96d65b31a6a54fb2778d37a487a7e9e9a337022
Reyaansh Sinha <reyaanshsinha4@gmail.com>
2025-08-24 17:20:55 -0400

    Update compiler.h for more function support

--- Compiler/compiler.h
+++ Compiler/compiler.h
@@ -4,9 +4,29 @@
 #include "../Parser/parser.h"
 #include <setjmp.h>
 
+// Extend ValueType to support objects for class instances
+// VAL_OBJECT: represents an instance of a class. When a class is instantiated
+// the returned value will have type VAL_OBJECT and its `object` pointer set.
+typedef enum { VAL_NUMBER, VAL_STRING, VAL_OBJECT } ValueType;
+// A Value can now hold either a numeric value, a string or an object pointer.
+// When type == VAL_NUMBER only the `number` field is valid.
+// When type == VAL_STRING only the `string` field is valid. The caller is
+// responsible for freeing the returned string.
+// When type == VAL_OBJECT only the `object` field is valid and points to
+// a ClassInstance structure defined in compiler.c.
+typedef struct {
+    ValueType type;
+    double number;
+    char *string;
+    void *object;
+} Value;
+
 extern jmp_buf return_buf;
-extern double return_value;
+extern Value return_value;
 
+Value eval(ASTNode *node);
 void interpret(ASTNode *node);
+char *eval_to_string(ASTNode *node);
+char *get_input_result();
 
-#endif // COMPILER_H
+#endif
